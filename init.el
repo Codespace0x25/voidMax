fc018b1b31eda2c94a541389c640b5d52b0b330f
Olivia <77221503+Codespace0x25@users.noreply.github.com>
2024-09-25 18:58:43 -0500

    Update init.el
    Removed deposits download from here as it is in the early init

--- init.el
+++ init.el
@@ -1,16 +1,4 @@
-;; Package setup
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(package-initialize)
-(package-refresh-contents)
-
-;; Ensure required packages are installed
-(dolist (pkg '(evil naga-theme lsp-mode  company flycheck lsp-java
-                    corfu kind-icon dashboard simple-modeline which-key
-                    vterm neotree))
-  (unless (package-installed-p pkg)
-    (package-install pkg)))
-
+;; there is no reason to do downloading in the gui init.
 ;; Key bindings
 (require 'evil)
 (evil-mode 1)
